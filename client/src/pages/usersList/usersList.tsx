22023aeb6a6ed393092b21d22cc6f84a9580f7ad
Mateusz Łuczak <luczakmmateusz@gmail.com>
2022-12-19 23:12:40 +0100

    feat: form with validation and connetion to server for adding users

--- client/src/pages/usersList/usersList.tsx
+++ client/src/pages/usersList/usersList.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import AddUser from '../../components/addUser/addUser';
 import Users from '../../components/users/users';
 
-const UsersList = () => <Users />;
+const UsersList = () => (
+  <>
+    <Users />
+    <AddUser />
+  </>
+);
 
 export default UsersList;
