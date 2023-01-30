4396a206a208b91613a2c934876b6f73b31adbf8
Mateusz Łuczak <82167694+xflameyoke@users.noreply.github.com>
2023-01-30 19:51:48 +0100

    Delete client/src/helpers directory
    fix: double files and folders

--- client/src/helpers/Urls.ts
+++
@@ -1,9 +0,0 @@
-export const url = {
-  login: 'http://localhost:3001/users/login',
-  users: 'http://localhost:3001/users',
-  changePIN: 'http://localhost:3001/users/changePIN',
-  menu: 'http://localhost:3001/menu',
-  auth: 'http://localhost:3001/users/auth',
-  shift: 'http://localhost:3001/shift',
-  orders: 'http://localhost:3001/orders'
-};
